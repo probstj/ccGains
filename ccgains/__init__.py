30b80ec8808d7fc704dbf14cf991982db9a2d75a
Jürgen Probst <juergen.probst@gmail.com>
2017-10-02 14:57:38 +0200

    added module and testing framework

---
+++ ccgains/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#
+# ----------------------------------------------------------------------
+# ccGains - Create capital gains reports for cryptocurrency trading.
+# Copyright (C) 2017 Jürgen Probst
+#
+# This file is part of ccGains.
+#
+# ccGains is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ccGains is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with ccGains. If not, see <http://www.gnu.org/licenses/>.
+# ----------------------------------------------------------------------
+#
+# Get the latest version at: https://github.com/probstj/ccGains
+#
+
---
+++ run_tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# discover and run all tests:
+python -m unittest discover
+
+# only a single test, e.g.:
+#python -m unittest tests.test_relations
+
