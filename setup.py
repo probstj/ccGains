6b8fc375b314bad11c43e7c5c4126613e14c85c0
Jürgen Probst <juergen.probst@gmail.com>
2017-10-09 14:04:36 +0200

    added setup script

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#
+# ----------------------------------------------------------------------
+# ccGains - Create capital gains reports for cryptocurrency trading.
+# Copyright (C) 2017 Jürgen Probst
+#
+# This file is part of ccGains.
+#
+# ccGains is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ccGains is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with ccGains. If not, see <http://www.gnu.org/licenses/>.
+# ----------------------------------------------------------------------
+#
+# Get the latest version at: https://github.com/probstj/ccGains
+#
+
+from setuptools import setup
+
+setup(name='ccGains',
+      version='1.0',
+      description='Python package for calculating cryptocurrency trading '
+                  'profits and creating capital gains reports',
+      url='https://github.com/probstj/ccgains',
+      author='Jürgen Probst',
+      author_email='juergen.probst@gmail.com',
+      license='LGPL-3.0-or-later',
+      packages=['ccgains'],
+      install_requires=[
+          'tables',
+          'numpy',
+          'pandas',
+      ],
+      zip_safe=False)
+
