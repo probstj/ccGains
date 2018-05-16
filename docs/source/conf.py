2bd0fd7ccdb391185207d362da59f9814c6cbf68
Jürgen Probst <juergen.probst@gmail.com>
2018-05-16 08:01:48 +0200

    added mock modules to sphinx config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,12 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
+import mock
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'tables',
+                'pandas', 'requests', 'jinja2', 'babel', 'weasyprint']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
 
 # -- Project information -----------------------------------------------------
 
